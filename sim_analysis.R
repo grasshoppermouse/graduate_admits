1bdd6e8ff02b75bc35f0186ad7ecf867563c3ff9
Ed Hagen <ehagen@gmail.com>
2023-03-22 06:02:55 -0700

    Tweak summary plot

--- sim_analysis.R
+++ sim_analysis.R
@@ -16,8 +16,27 @@ d_sum <-
     N = n()
   )
 
+d_median <-
+  d_sum %>% 
+  group_by(course) %>% 
+  summarise(
+    median = median(N)
+  ) %>% 
+  mutate(
+    course = fct_reorder(course, median)
+  )
+
+d_sum$course <- factor(d_sum$course, levels = levels(d_median$course))
+
 # Elective course numbers must be divided by the average number
 # of elective courses offered each semester
 ggplot(d_sum, aes(N, course)) +
   geom_count() +
+  geom_point(data = d_median, aes(median, course), colour = 'red', shape = 3, size = 5) +
+  labs(
+    title = 'Course enrollment distribution',
+    subtitle = 'Red cross: median enrollment',
+    x = '\nEnrollment',
+    y = ''
+  ) +
   theme_minimal(15)
