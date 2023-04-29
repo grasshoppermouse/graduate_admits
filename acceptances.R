c828df967d32accb2c7801d911fb34086beb57af
Ed Hagen <ehagen@gmail.com>
2023-04-29 14:11:22 -0700

    Added plot variant

--- acceptances.R
+++ acceptances.R
@@ -18,6 +18,17 @@ ggplot(d, aes(Year, Admits, colour=Stream, group=Stream)) +
   geom_line(linewidth=2) +
   theme_minimal(15)
 
+ggplot(d[d$Stream != 'Total',], aes(Year, Admits, fill=Stream)) +
+  geom_col() +
+  scale_x_continuous(breaks=2014:2022) +
+  scale_fill_viridis_d(end = 0.9, option = 'G') +
+  labs(
+    title='Anthropology graduate admissions',
+    x = '',
+    y = 'Number of admissions'
+  ) +
+  theme_minimal(15)
+
 mean_arch <- mean(d0$Arch[d0$Year %in% 2020:2022])
 mean_cult <- mean(d0$Cult[d0$Year %in% 2020:2022])
 mean_evo  <- mean(d0$Evo[d0$Year %in% 2020:2022])
